38f8f631576dc0a6c19fac52a3e5398aa50e089b
pomelov <rom4ik@e-kirov.ru>
2020-01-05 12:43:56 +0300

    скрипт для поиска самого дружного друга

---
+++ vk-friends-max.py
@@ -0,0 +1,26 @@
+import requests
+import json
+import time
+
+f = open('token.txt', 'r')
+token = f.read()
+f.close()
+
+# получить список своих друзей
+params = {'v': '5.52', 'access_token': token}
+r = requests.get('https://api.vk.com/method/friends.get', params=params)
+friends = json.loads(r.content)
+
+max_count = 0
+max_user_id = 0
+for friend in friends['response']['items']:
+    params = {'user_id': friend, 'v': '5.52', 'access_token': token}
+    r = requests.get('https://api.vk.com/method/friends.get', params=params)
+    f = json.loads(r.content)
+    count = f['response']['count']  # получить число друзей у друга
+    if count > max_count:
+        max_count = count
+        max_user_id = friend
+    time.sleep(0.01)
+
+print(max_user_id, max_count)
