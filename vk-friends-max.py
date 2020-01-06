fa55a351f4d4fdfcf20ef02bd7022653e33b9ce5
pomelov <rom4ik@e-kirov.ru>
2020-01-06 11:53:24 +0300

    ограничить список друзей до 10

--- vk-friends-max.py
+++ vk-friends-max.py
@@ -7,7 +7,12 @@ token = f.read()
 f.close()
 
 # получить список своих друзей
-params = {'v': '5.52', 'access_token': token}
+params = {
+    'v': '5.52', 
+    'access_token': token, 
+    'count': 10, 
+    'order': 'hints'
+}
 r = requests.get('https://api.vk.com/method/friends.get', params=params)
 friends = json.loads(r.content)
 
@@ -21,6 +26,8 @@ for friend in friends['response']['items']:
     if count > max_count:
         max_count = count
         max_user_id = friend
-    time.sleep(0.01)
+    print('.', end='')
+    time.sleep(1)
 
+print()
 print(max_user_id, max_count)
