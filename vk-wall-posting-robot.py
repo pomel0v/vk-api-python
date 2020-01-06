9570fc8e9c4a93276e99d87bfa612a3777678eff
pomelov <rom4ik@e-kirov.ru>
2020-01-06 12:50:29 +0300

    скрипт для постинга на стене ВК

---
+++ vk-wall-posting-robot.py
@@ -0,0 +1,20 @@
+import requests
+import json
+import time
+import random
+
+f = open('token.txt', 'r')
+token = f.read()
+f.close()
+
+params = {
+    'v': '5.52',
+    'access_token': token,
+    'owner_id': 382533516,
+    'message': random.randint(1, 10000000),
+    'attachments': 'photo382533516_457240293',
+    'lat': 55.753663,
+    'long': 37.619854,
+}
+
+r = requests.get('https://api.vk.com/method/wall.post', params=params)
